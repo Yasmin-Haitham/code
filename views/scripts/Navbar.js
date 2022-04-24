666ff5b5b867b10990cd39d3f84e7ccb6d753117
MoYasser <mohamed.yasser2000@ymail.com>
2022-04-24 03:59:31 +0200

    Added Boostrap Navbar HTML done

--- views/scripts/Navbar.js
+++ views/scripts/Navbar.js
@@ -1,22 +1,26 @@
 var navbar =`
+<link  href="../styles/navbar.css" rel="stylesheet">
 <nav>
     <div>
-        <a href="/">LOGO</a>
+        <a href="/">
+            <img src="../tests/mylogo.png"></img>
+        </a>
     </div>
-    <div>
-        <a href="/views/homepage.html">Homepage</a>
-        <a href="/views/login.html">Login</a>
-        <a href="/views/signup.html"></a>
-        
-        <!-- (middleware) need to be authenticated 
-        using jQuery selectors?
-        $("h1#id").method -->
-        
-        <a href="/views/user_view.html">Update</a>
-        <a href="/views/library.html">Library</a>
-        <!-- (middleware) need to be authorized -->
-        <a href="/views/members.html">Members</a>
+    <div style="float:right" >
+        <ul>
+            <li> <a href="/views/homepage.html">Homepage</a> </li>
+            <li> <a href="/views/login.html">Login</a> </li>
+            <li> <a href="/views/signup.html">Signup</a> </li>
+
+            <!-- (middleware) need to be authenticated 
+            using jQuery selectors?
+            $("h1#id").method -->
+
+            <li> <a href="/views/user_view.html">Update</a> </li>
+            <li> <a href="/views/library.html">Library</a> </li>
+            <!-- (middleware) need to be authorized -->
+            <li> <a href="/views/members.html">Members</a> </li>
+        </ul>
     </div>
-    <hr>
 </nav>`;
 document.body.insertAdjacentHTML("afterbegin",navbar)
