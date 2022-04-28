da42fc76784dbe85fe989d95133ecc538d2a045d
Yasmin-Haitham <66221948+Yasmin-Haitham@users.noreply.github.com>
2022-04-28 14:24:59 +0200

    logo changed

--- views/scripts/Navbar.js
+++ views/scripts/Navbar.js
@@ -2,7 +2,7 @@ var navbar =`
 <link  href="../styles/navbar.css" rel="stylesheet">
 <nav class="navbar navbar-expand-lg navbar-light" >
     <div class="container-fluid">
-        <a class="navbar-brand" id="logo" href="/" style="width = 15%"> <img src="../public/mylogo.png" alt="book logo"/></a>
+        <a class="navbar-brand" id="logo" href="/" style="width = 15%"> <img src="../public/mylogo1.png" alt="book logo"/></a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation" display= "flex-end"><span class="navbar-toggler-icon" ></span></button>
         <div class="collapse jus navbar-collapse " id="navbarNavAltMarkup">
